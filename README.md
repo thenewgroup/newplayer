e4529bd10eee8fec1071fc664ecdeb6b08b8b3c9
Kory Paulsen <kory.paulsen@thenewgroup.com>
2014-11-05 14:32:51 -0800

    Button component - support for external links

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ newplayer
 =========
 
 Responsive learning content player by www.thenewgroup.com
+
+
+
+Dev Notes:
+
+High-level TBDs:
+
+- fix lang/page routing
+	- lang is OPTIONAL
+		- route must assume pageId if lang AND pageId are not both provided
+	- default determiniation (if only manifest is specified)
+		- Router needs variable that triggers reroute if populated
+		- Content & Page components need hooks
+			- postParse( manifest ) after initial manifest parse
+				- Content.postParse appends lang (or blank if only one) to reroute var
+				- Page.postParse appends pageId to reroute var
+
+- finish menu component
+	- Menu controller can getAll('Page')
+
+- fix Column component or fork to Grid component
+	- ltr flow, calc remaining widths for sub-components
+
+- pass component data via attribute in npComponent directive (don't pull from $scope)
+
+- whitelist core components and bake 'em into main script
+	- try to keep them agnostic of eachother
+	- component service doesn't load external js for whitelisted core components
+		
+
--- app/scripts/component/Button/Button.js
+++ app/scripts/component/Button/Button.js
@@ -14,16 +14,35 @@ angular
 		function( $log, $scope, $sce, $location )
 		{
 			var cmpData = $scope.component.data;
+			$log.debug( 'Button::data', cmpData );
 
-			$log.debug( 'Button::scope', $scope );
-			this.content = $sce.trustAsHtml( cmpData.output );
-			if ( !!cmpData.link )
+			this.content = '';
+			var btnContent = cmpData.output;
+			if ( !!btnContent && typeof( btnContent ) === 'string' )
 			{
-				this.link = $sce.trustAsResourceUrl( cmpData.link );
+				this.content = $sce.trustAsHtml( btnContent );
+			}
+
+			this.link = '';
+			this.linkInternal = false;
+			var btnLink = cmpData.link;
+			if ( !!btnLink && typeof( btnLink ) === 'string' )
+			{
+				if ( btnLink.indexOf( '#' ) === 0 )
+				{
+					this.linkInternal = true;
+					btnLink = btnLink.substr(1);
+				}
+				this.link = $sce.trustAsResourceUrl( btnLink );
 				$log.debug( 'Button::link', this.link );
 			}
 			this.go = function() {
-				$location.path( this.link );
+				if ( this.linkInternal )
+				{
+					$location.url( this.link );
+				} else {
+					document.location = this.link;
+				}
 			}
 		}
 	)
