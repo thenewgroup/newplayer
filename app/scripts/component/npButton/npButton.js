f8ed25685c50cea8b089caab651ef2251542e7aa
Kory Paulsen <kory.paulsen@thenewgroup.com>
2014-11-19 18:11:32 -0800

    Bugfix - fixed button links to url outside of app

--- app/scripts/component/npButton/npButton.js
+++ app/scripts/component/npButton/npButton.js
@@ -30,18 +30,19 @@ angular
 			var btnLink = cmpData.link;
 			if ( !!btnLink && typeof( btnLink ) === 'string' )
 			{
-				if ( btnLink.indexOf( '/' ) === 0 )
+				if ( btnLink.indexOf( '/' ) === 0 || /^https?:\/\//.test( btnLink ) )
 				{
 					this.linkInternal = false;
-				}
-				if ( btnLink.indexOf( '#' ) === 0 )
-				{
-					btnLink = btnLink.substr(1);
 				} else {
-					btnLink = '/' + ConfigService.getManifestId() + '/' + btnLink;
+					if ( btnLink.indexOf( '#' ) === 0 )
+					{
+						btnLink = btnLink.substr(1);
+					} else {
+						btnLink = '/' + ConfigService.getManifestId() + '/' + btnLink;
+					}
 				}
+				$log.debug( 'npButton::link', btnLink );
 				this.link = $sce.trustAsResourceUrl( btnLink );
-				$log.debug( 'npButton::link', this.link );
 			}
 			this.go = function() {
 				if ( this.linkInternal )
