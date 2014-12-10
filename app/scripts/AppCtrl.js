c64266c63cc3ce20a4cbc63bc9f556e2ff37f6b1
Dave West <dave@unleashed-software.com>
2014-12-09 21:00:13 -0800

    intial go of assessment service

--- app/scripts/AppCtrl.js
+++ app/scripts/AppCtrl.js
@@ -1,9 +1,411 @@
 'use strict';
 
 /** @ngInject */
-function AppController( $log/*, ImagePreloadFactory, HomeService, $scope*/ )
+function AppController( $log, AssessmentService/*, ImagePreloadFactory, HomeService, $scope*/ )
 {
 	$log.debug('AppController::Init');
+
+
+        AssessmentService.setRequirements(10,5,0.8);
+
+        $log.info('Initial------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake non-required', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.pageViewed('fake non-required', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+        AssessmentService.pageViewed('fake non-required', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.pageViewed('fake non-required', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+        AssessmentService.pageViewed('fake non-required', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.pageViewed('fake non-required', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+        AssessmentService.pageViewed('fake non-required', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.pageViewed('fake non-required', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+        AssessmentService.pageViewed('fake non-required', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.pageViewed('fake non-required', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+        AssessmentService.questionCorrectlyAnswered('fq', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+        AssessmentService.pageViewed('fake non-required', false);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+        AssessmentService.pageViewed('fake required', true);
+
+        $log.info('------------------------');
+        $log.info('Current Score', AssessmentService.getScore());
+        $log.info('Passing', AssessmentService.isPassing());
+
+
+
+        $log.info('Page stats', AssessmentService.getPageStats());
+        $log.info('Question stats', AssessmentService.getQuestionStats());
+
+
 	/*
     var loader = ImagePreloadFactory.createInstance();
     loader.addImages([
