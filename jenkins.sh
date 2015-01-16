2464e697a671ab58abf8caef367a1b3b31763b41
Kory Paulsen <kory.paulsen@thenewgroup.com>
2015-01-16 11:55:27 -0800

    jenkins script step indicators

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,13 @@
 cd $WORKSPACE
+echo -e "\n\nnpm install -g bower"
 npm install -g bower
+echo -e "\n\nnpm install -g grunt-cli"
 npm install -g grunt-cli
+echo -e "\n\nbower install"
 bower install
+echo -e "\n\nnpm install"
 npm install
+echo -e "\n\nbundle install"
 bundle install
+echo -e "\n\ngrunt build"
 grunt build
