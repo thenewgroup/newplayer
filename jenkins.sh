a725da877d0990e8e80b7cf2c8ecac000a46d82c
Greg Boggs <gregboggs@153069.no-reply.drupal.org>
2015-01-16 15:29:19 -0800

    remove sudo which should not be needed now

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,12 @@
 cd $WORKSPACE
 echo "\n\nnpm install -g bower"
-sudo npm install -g bower
+npm install -g bower
 echo "\n\nnpm install -g grunt-cli"
-sudo npm install -g grunt-cli
+npm install -g grunt-cli
 echo "\n\nbower install"
-sudo bower install
+bower install
 echo "\n\nnpm install"
-sudo npm install
+npm install
 echo "\n\nbundle install"
 bundle install
 echo "\n\ngrunt build"
