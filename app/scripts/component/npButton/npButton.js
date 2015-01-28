08f218dd0a3036f119da0b6f49d541cf20d19f52
christian worley <christian@worleydev.com>
2015-01-28 14:05:58 -0800

    carat SVG animation changes

--- app/scripts/component/npButton/npButton.js
+++ app/scripts/component/npButton/npButton.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular
-	.module(
-		'npButton',
-		[]
-		);
         .module(
                 'npButton',
                 []
@@ -71,53 +67,11 @@ angular
                 }
         )
 
-			this.link = '';
-			this.target = cmpData.target;
-			this.linkInternal = true;
-			var btnLink = cmpData.link;
-			if ( angular.isString( btnLink ) )
-			{
-				if ( btnLink.indexOf( '/' ) === 0 )
-				{
-					if ( ! this.target )
-					{
-						this.target = '_top';
-					}
-					this.linkInternal = false;
-				} else
-				if ( /^([a-zA-Z]{1,10}:)?\/\//.test( btnLink ) )
-				{
-					if ( ! this.target )
-					{
-						this.target = '_blank';
-					}
-					this.linkInternal = false;
-				} else {
-					if ( btnLink.indexOf( '#' ) === 0 )
-					{
-						btnLink = btnLink.substr(1);
-					} else {
-						btnLink = '/' + ConfigService.getManifestId() + '/' + btnLink;
-					}
-				}
-				$log.debug( 'npButton::link', btnLink );
-				this.link = $sce.trustAsResourceUrl( btnLink );
-			}
-			this.go = function() {
-				if ( this.linkInternal )
-				{
-					$location.url( this.link );
-				} else {
-					window.open( this.link, this.target );
-				}
-			};
-		}
-	)
+        /** @ngInject */
+        .run(
+                function ($log, $rootScope)
+                {
+                    $log.debug('npButton::component loaded!');
+                }
+        );
 
-	/** @ngInject */
-	.run(
-		function ($log, $rootScope)
-		{
-			$log.debug('npButton::component loaded!');
-		}
-	);
