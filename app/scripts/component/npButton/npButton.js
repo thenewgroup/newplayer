9bf96eb93b17970e41cbdba58e2575dd6a24975f
Dave West <dave@unleashed-software.com>
2015-02-13 16:10:38 -0800

    npButton should use npStatus for internal links

--- app/scripts/component/npButton/npButton.js
+++ app/scripts/component/npButton/npButton.js
@@ -1,77 +1,67 @@
 'use strict';
 
 angular
-	.module(
-		'npButton',
-		[ ]
-	);
+  .module(
+  'npButton',
+  ['newplayer.service']
+);
 
 angular
-	.module('npButton')
+  .module('npButton')
 
-	/** @ngInject */
-	.controller( 'npButtonController',
-		function( $log, $scope, $sce, $location, $element, ConfigService )
-		{
-			var cmpData = $scope.component.data || {};
-			$log.debug( 'npButton::data', cmpData );
+/** @ngInject */
+  .controller('npButtonController',
+  function ($log, $scope, $sce, $location, $element, ConfigService, npState) {
+    var cmpData = $scope.component.data || {};
+    $log.debug('npButton::data', cmpData);
 
-			this.content = '';
-			var btnContent = cmpData.content;
-			if ( angular.isString( btnContent ) )
-			{
-				this.content = $sce.trustAsHtml( btnContent );
-				//$element.append( btnContent );
-			}
+    this.content = '';
+    var btnContent = cmpData.content;
+    if (angular.isString(btnContent)) {
+      this.content = $sce.trustAsHtml(btnContent);
+      //$element.append( btnContent );
+    }
 
-			this.link = '';
-			this.target = cmpData.target;
-			this.linkInternal = true;
-			var btnLink = cmpData.link;
-			if ( angular.isString( btnLink ) )
-			{
-				if ( btnLink.indexOf( '/' ) === 0 )
-				{
-					if ( ! this.target )
-					{
-						this.target = '_top';
-					}
-					this.linkInternal = false;
-				} else
-				if ( /^([a-zA-Z]{1,10}:)?\/\//.test( btnLink ) )
-				{
-					if ( ! this.target )
-					{
-						this.target = '_blank';
-					}
-					this.linkInternal = false;
-				} else {
-					if ( btnLink.indexOf( '#' ) === 0 )
-					{
-						btnLink = btnLink.substr(1);
-					} else {
-						btnLink = '/' + ConfigService.getManifestId() + '/' + btnLink;
-					}
-				}
-				$log.debug( 'npButton::link', btnLink );
-				this.link = $sce.trustAsResourceUrl( btnLink );
-			}
-			this.go = function() {
-				if ( this.linkInternal )
-				{
-					$location.url( this.link );
-				} else {
-					window.open( this.link, this.target );
-				}
-			};
-		}
-	)
+    this.link = '';
+    this.target = cmpData.target;
+    this.linkInternal = true;
+    var btnLink = cmpData.link;
+    if (angular.isString(btnLink)) {
+      if (btnLink.indexOf('/') === 0) {
+        if (!this.target) {
+          this.target = '_top';
+        }
+        this.linkInternal = false;
+      } else if (/^([a-zA-Z]{1,10}:)?\/\//.test(btnLink)) {
+        if (!this.target) {
+          this.target = '_blank';
+        }
+        this.linkInternal = false;
+      } else {
+        if (btnLink.indexOf('#') === 0) {
+          btnLink = btnLink.substr(1);
+        } else {
+          btnLink = '/' + ConfigService.getManifestId() + '/' + btnLink;
+        }
+      }
+      $log.debug('npButton::link', btnLink);
+      this.link = $sce.trustAsResourceUrl(btnLink);
+    }
+    this.go = function () {
+      if (this.linkInternal) {
+        //$location.url(this.link);
+        npState.showPageId(cmpData.id);
+      } else {
+        window.open(this.link, this.target);
+      }
+    };
+  }
+)
 
-	/** @ngInject */
-	.run(
-		function( $log, $rootScope )
-		{
-			$log.debug('npButton::component loaded!');
-		}
-	);
+/** @ngInject */
+  .run(
+  function ($log, $rootScope) {
+    $log.debug('npButton::component loaded!');
+  }
+);
 
