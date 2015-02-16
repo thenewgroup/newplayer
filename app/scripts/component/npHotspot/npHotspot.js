2ef81314c2446cf17591bc18976e7f2fed1afa72
christian worley <christian@worleydev.com>
2015-02-16 13:14:05 -0800

    image to background image changes/responsive content area changes

--- app/scripts/component/npHotspot/npHotspot.js
+++ app/scripts/component/npHotspot/npHotspot.js
@@ -22,10 +22,11 @@ angular
                 function ($log, $scope, $sce, $element) {
                     var cmpData = $scope.component.data;
                     var buttonData = $scope.feedback || {};
+                    var contentAreaHeight;
                     $log.debug('npHotspot::data', cmpData, buttonData);
 
                     var hotspotButtons = '';
-                    this.hotspotButtons = cmpData.hotspotButtons; 
+                    this.hotspotButtons = cmpData.hotspotButtons;
 
                     this.id = cmpData.id;
                     this.baseURL = cmpData.baseURL;
@@ -35,8 +36,29 @@ angular
                     $scope.image = this.image = cmpData.image;
 
                     this.update = function (button) {
-                        var idx = this.hotspotButtons.indexOf(button);
                         this.feedback = button.feedback;
+                        var idx = this.hotspotButtons.indexOf(button);
+
+                        //////////////////////
+                        $scope.$watch('npHotspot.feedback', function (newValue, oldValue) {
+                            $('.npHotspot-feedback p').each(function (index, totalArea) {
+                                var contentAreaHeight = $(this).outerHeight(true) + 50;
+                                TweenMax.to($('.content-background'), 1, {
+                                    height: contentAreaHeight,
+                                    ease: Power4.easeOut
+                                });
+                                TweenMax.to($('.npHotspot-feedback'), 0.1, {
+                                    opacity: 0,
+                                    ease: Power4.easeOut
+                                });
+                                TweenMax.to($('.npHotspot-feedback'), 0.5, {
+                                    delay: 0.5,
+                                    opacity: 1,
+                                    ease: Power4.easeOut
+                                });
+                            });
+                        });
+                        //////////////////////
                     };
                 }
         )
