144bde2cde0ebfb00ab65696c6ba6252ca1ac8b9
Michael Wills <design@michaelangela.name>
2015-02-17 03:11:22 -0800

    create ngClick based on link property

--- app/scripts/component/npHTML/npHTML.js
+++ app/scripts/component/npHTML/npHTML.js
@@ -13,11 +13,18 @@ angular
 	.controller( 'npHTMLController',
 		function( $log, $scope, $sce )
 		{
-			var cmpData = $scope.component.data;
+			var cmpData = $scope.component.data,
+        content = null;
 			$log.debug( 'npHTML::data', cmpData );
 
+      // TODO: fix me! We need to make an internal link instead of an external href
+      if (cmpData.link) {
+        content = '<a href="" ng-click="vm.update(' + cmpData.link + ')">' + angular.element(cmpData.content).text() + '</a>';
+      } else {
+        content = cmpData.content;
+      }
 			this.content = $sce.trustAsHtml( cmpData.content );
-			$log.debug( 'npHTML::content', $scope.content );
+			$log.debug( 'npHTML::content', $scope.content, this.content, cmpData.link );
 		}
 	)
 
