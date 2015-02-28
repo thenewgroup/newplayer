bf7ed68fd8f38059b3dd7f7621a9533bfaa851cb
christian worley <christian@worleydev.com>
2015-02-27 16:09:52 -0800

    formatting/jshint error squash

--- app/scripts/component/npHTML/npHTML.js
+++ app/scripts/component/npHTML/npHTML.js
@@ -1,9 +1,7 @@
 (function () {
-
   'use strict';
   angular
     .module('newplayer.component')
-
   /** @ngInject */
     .controller('npHTMLController',
     function ($log, $scope, $rootScope) {
@@ -11,26 +9,21 @@
         cmpData = $scope.component.data,
         content = null;
       $log.debug('npHTML::data', cmpData);
-
       if (cmpData.link) {
         this.link = cmpData.link;
       }
-
       this.content = cmpData.content;
       $log.info('npHTML::content', $scope.content, this.content, cmpData.link);
-
       this.handleLink = function() {
         $log.info('npHTML:handleLink | link is a manifest');
         $rootScope.$broadcast('npReplaceManifest', cmpData.link);
-      }
+      };
     }
   )
-
   /** @ngInject */
     .run(
     function ($log, $rootScope) {
       $log.debug('npHTML::component loaded!');
     }
   );
-
 })();
