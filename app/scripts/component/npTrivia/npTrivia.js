023e0fc915061feef3e52b0df9259d3b22fb42ea
Michael Wills <design@michaelangela.name>
2015-03-03 10:28:54 -0800

    docs(npTrivia): TODO language string from app

--- app/scripts/component/npTrivia/npTrivia.js
+++ app/scripts/component/npTrivia/npTrivia.js
@@ -21,24 +21,25 @@
       vm.seenComponents = _.shuffle($scope.components);
       vm.pageId = vm.seenComponents[0].data.id;
       vm.difficulty = vm.seenComponents[0].components[0].data.difficulty || 0;
-     
+
       // go to the first page, since pages were shuffled
       $timeout(function () {
-    	  ManifestService.setPageId(vm.pageId)
+        ManifestService.setPageId(vm.pageId);
       });
 
       $rootScope.$on('question.answered', function (evt, correct) {
       	if (correct) {
     			vm.assment.pageViewed();
       		vm.currentPage = vm.assment.getPageStats().viewed.total;
-		      vm.pageId = vm.seenComponents[vm.currentPage] ? 
+		      vm.pageId = vm.seenComponents[vm.currentPage] ?
 		      	vm.seenComponents[vm.currentPage].data.id : '';
 	    	  ManifestService.setPageId(vm.pageId);
 					$rootScope.$emit('spin-to-win');
 
 	    	  // end of the trivia questions
-	    	  // TODO - add this message the template and set the two values 
+	    	  // TODO - add this message the template and set the two values
 	    	  // here in the controller
+          // NOTE: This text should come from the app
 	    	  if (!vm.pageId) {
 	    	  	vm.feedback = 'Good job, you scored 5,000 points out of 7,500 possible.';
 	    	  }
