ed1abe08eded60518c6d4b30860019705d328d39
Steven Irby <steven.a.irby@gmail.com>
2015-03-09 20:01:16 -0700

    Added new component npMatch for matching game.

---
+++ app/scripts/component/npMatch/npMatch.js
@@ -0,0 +1,113 @@
+(function () {
+
+  'use strict';
+  angular
+    .module('newplayer.component')
+  /** @ngInject */
+    .controller('npMatchController',
+    function ($log, $scope, $rootScope, ManifestService, $sce) {
+      var cmpData = $scope.component.data;
+      $log.debug('npQuestion::data', cmpData);
+
+      this.id = cmpData.id;
+      this.content = $sce.trustAsHtml(cmpData.content);
+      this.type = cmpData.type;
+      this.feedback = '';
+      this.canContinue = false;
+
+      var feedback = cmpData.feedback;
+
+      this.changed = function () {
+        $log.debug('npQuestion::answer changed');
+        if (feedback.immediate) {
+          this.feedback = '';
+        }
+      };
+
+      this.evaluate = function () {
+        var correct = true;
+        $log.debug('npQuestion::evaluate:', this.answer);
+        if (!!this.answer) {
+          switch (this.type) {
+            case 'radio':
+              var radAnswer = ManifestService.getComponent(this.answer);
+              if (angular.isString(radAnswer.data.feedback)) {
+                this.feedback = radAnswer.data.feedback;
+              }
+              correct = radAnswer.data.correct;
+              break;
+            case 'checkbox':
+              var chkAnswers = ManifestService.getAll('npAnswer', $scope.cmpIdx);
+              var idx;
+              for (idx in chkAnswers) {
+                if (chkAnswers[idx].data.correct) {
+                  // confirm all correct answers were checked
+                  if (!this.answer[chkAnswers[idx].idx]) {
+                    correct = false;
+                  }
+                } else {
+                  // confirm no incorrect answers were checked
+                  if (this.answer[chkAnswers[idx].idx]) {
+                    correct = false;
+                  }
+                }
+              }
+              break;
+            case 'text':
+              var txtAnswer = ManifestService.getFirst('npAnswer', $scope.cmpIdx);
+              var key = txtAnswer.data.correct;
+              var regExp, pat, mod = 'i';
+              if (angular.isString(key)) {
+                if (key.indexOf('/') === 0) {
+                  pat = key.substring(1, key.lastIndexOf('/'));
+                  mod = key.substring(key.lastIndexOf('/') + 1);
+                }
+              } else if (angular.isArray(key)) {
+                pat = '^(' + key.join('|') + ')$';
+              }
+              regExp = new RegExp(pat, mod);
+              if (!regExp.test(this.answer)) {
+                if (angular.isObject(txtAnswer.data.feedback) && angular.isString(txtAnswer.data.feedback.incorrect)) {
+                  this.feedback = txtAnswer.data.feedback.incorrect;
+                }
+                correct = false;
+              } else {
+                if (angular.isObject(txtAnswer.data.feedback) && angular.isString(txtAnswer.data.feedback.correct)) {
+                  this.feedback = txtAnswer.data.feedback.correct;
+                }
+              }
+              break;
+          }
+        } else {
+          correct = false;
+        }
+        $log.debug('npQuestion::evaluate:isCorrect', correct);
+
+        // set by ng-model of npAnswer's input's
+        if (feedback.immediate && this.feedback === '') {
+          if (correct) {
+            this.feedback = feedback.correct;
+            this.canContinue = true;
+          } else {
+            this.feedback = feedback.incorrect;
+            this.canContinue = false;
+          }
+        }
+      };
+
+      this.nextPage = function (evt) {
+        evt.preventDefault();
+        if (this.canContinue) {
+          $rootScope.$emit('question.answered', true);
+        }
+      };
+    }
+  )
+
+  /** @ngInject */
+    .run(
+    function ($log, $rootScope) {
+      $log.debug('npQuestion::component loaded!');
+    }
+  );
+})();
