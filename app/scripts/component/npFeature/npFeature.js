12cf19f860d155f0c859e972427f86efa4b68c42
christian worley <christian@worleydev.com>
2015-03-24 17:49:37 -0700

    build changes

--- app/scripts/component/npFeature/npFeature.js
+++ app/scripts/component/npFeature/npFeature.js
@@ -13,39 +13,38 @@
                 return function ($scope, $element, attrs) {
                     setTimeout(function () {
                         $scope.$apply(function () {
-                            var np_wrapper = $element.find('.np_outside-padding');
-                            var hotspotImage = $element.find('.hotspotImage');
-//                            var page_container = $element.find('.modal .modal-open');
-                            var page_container = $element.find('.modal-open');
-                            var page_container = $('.modal-open');
-                            TweenMax.to(np_wrapper, 0.25, {
-                                autoAlpha: 0.25,
-                                ease: Power2.easeOut
-                            });
-                            function scroller() {
-                                console.log(
-                                        '\n::::::::::::::::::::::::::::::::::::::atTop::atTop:::::::::::::::::::::::::::::::::::::::::::::::::',
-                                        '\n::page_container::', page_container,
-                                        '\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::'
-                                        );
-                                TweenMax.to(page_container, .75, {
-                                    scrollTo: {y: 0},
-                                    ease: Power2.easeInOut,
-                                    onComplete: atTop
-                                });
-                            }
-                            function atTop() {
+//                            var np_wrapper = $element.find('.np_outside-padding');
+//                            var hotspotImage = $element.find('.hotspotImage');
+//                            var page_container = $element.find('.modal-open');
+//                            var page_container = $('.modal-open');
+//                            TweenMax.to(np_wrapper, 0.25, {
+//                                autoAlpha: 0.25,
+//                                ease: Power2.easeOut
+//                            });
+//                            function scroller() {
 //                                console.log(
 //                                        '\n::::::::::::::::::::::::::::::::::::::atTop::atTop:::::::::::::::::::::::::::::::::::::::::::::::::',
 //                                        '\n::page_container::', page_container,
 //                                        '\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::'
 //                                        );
-                                TweenMax.to(np_wrapper, 0.5, {
-                                    autoAlpha: 1,
-                                    ease: Power2.easeOut
-                                });
-                            }
-                            scroller();
+//                                TweenMax.to(page_container, .75, {
+//                                    scrollTo: {y: 0},
+//                                    ease: Power2.easeInOut,
+//                                    onComplete: atTop
+//                                });
+//                            }
+//                            function atTop() {
+//                                console.log(
+//                                        '\n::::::::::::::::::::::::::::::::::::::atTop::atTop:::::::::::::::::::::::::::::::::::::::::::::::::',
+//                                        '\n::page_container::', page_container,
+//                                        '\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::'
+//                                        );
+//                                TweenMax.to(np_wrapper, 0.5, {
+//                                    autoAlpha: 1,
+//                                    ease: Power2.easeOut
+//                                });
+//                            }
+//                            scroller();
                         });
                     });
                 };
