319808cc5c494c9bf3971c69cf9029ccabee0cef
christian worley <christian@worleydev.com>
2015-04-11 18:34:01 -0700

    Style updates

--- app/scripts/component/npVideo/npVideo.js
+++ app/scripts/component/npVideo/npVideo.js
@@ -1,12 +1,10 @@
 /* jshint -W003, -W117 */
 (function () {
     'use strict';
-
     angular
             .module('newplayer.component')
             /** @ngInject */
             .directive('npVideo', NpVideoDirective);
-
     /** @ngInject */
     function NpVideoDirective($log) {
         $log.info('DEBUG | \npVideo::Init\n');
@@ -17,16 +15,22 @@
             bindToController: true
         };
     }
-
     /** @ngInject */
     function NpVideoController($log, $scope, $sce) {
-
         var vm = this,
-                types = $scope.component.data.types;
-
+                types = $scope.component.data.types,
+                source = $scope.component.data.baseURL;
         if (angular.isArray(types) && types.length > 0) {
             var sources = [];
             for (var typeIdx in types) {
+//                console.log(
+//                        '\n::::::::::::::::::::::::::::::::::::::npQuestions::evaluate:::::::::::::::::::::::::::::::::::::::::::::::::',
+//                        '\n::$scope::', $scope,
+//                        '\n::source::', source,
+//                        '\n::$scope.component.data::', $scope.component.data,
+//                        '\n::$scope.component.data.baseURL::', $scope.component.data.baseURL,
+//                        '\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::'
+//                        );
                 var type = types[typeIdx];
                 sources.push({
                     type: type,
@@ -39,5 +43,4 @@
             };
         }
     }
-
 })();
