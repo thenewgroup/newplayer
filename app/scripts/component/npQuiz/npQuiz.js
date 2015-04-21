6ce07372b4856d7d4a9f78615197e8021998d989
Dave West <dave@unleashed-software.com>
2015-04-20 20:32:54 -0700

    feat(quiz): quiz should add 'see results' to manifests with `npAsResult`s

--- app/scripts/component/npQuiz/npQuiz.js
+++ app/scripts/component/npQuiz/npQuiz.js
@@ -6,7 +6,7 @@
   /** @ngInject */
     .controller('npQuizController',
     function ($log, $scope, AssessmentService) {
-      var minPassing, cmpData = $scope.component.data;
+      var minPassing, i, j, lastComponent, lastComponentIndex, nLastComponent, nLastComponentIndex, cmpData = $scope.component.data;
       $log.debug('npQuiz::data', cmpData);
 
       if( cmpData.hasOwnProperty('assessed') && parseInt(cmpData.assessed) === 1 ) {
@@ -15,6 +15,38 @@
 
           if( minPassing > 1) {
             minPassing = minPassing / 100;
+          }
+        }
+
+        var getResultsBtn = {
+          "type": "npButton",
+          "data": {
+            "link": "",
+            "type": "btn-next",
+            "class": "",
+            "content": "See Results"
+          },
+          "components": [
+          ]
+        };
+
+        // add the results button if the last page is a npAsResult
+        lastComponentIndex = $scope.component.components.length - 1;
+        if( lastComponentIndex >= 0 ) {
+          lastComponent = $scope.component.components[lastComponentIndex];
+
+
+          if( lastComponent ) {
+            for( i = 0; i < lastComponent.components.length; i++ ) {
+              $log.debug('npQuiz: looking for npAsResult', lastComponent.components[i]);
+              if( lastComponent.components[i].type === 'npAsResult' ) {
+                nLastComponentIndex = $scope.component.components.length - 2;
+
+                if( nLastComponentIndex >= 0 ) {
+                  $scope.component.components[nLastComponentIndex].components.push(getResultsBtn);
+                }
+              }
+            }
           }
         }
 
