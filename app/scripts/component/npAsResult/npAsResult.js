1fb6004cc09133edd236cf8b760a3cfb76a0e60b
christian worley <christian@worleydev.com>
2015-04-22 14:50:41 -0700

    round percentage

--- app/scripts/component/npAsResult/npAsResult.js
+++ app/scripts/component/npAsResult/npAsResult.js
@@ -67,7 +67,7 @@
                         // replace tokens in the string as we go
                         vm.summaryText = vm.summaryText.replace(/:USERSCORE:/, $filter('number')(vm.score * 100, 0));
                         vm.summaryText = vm.summaryText.replace(/:MINSCORE:/, $filter('number')(vm.minScore * 100, 0));
-                        vm.summaryPecentage = (vm.score * 100);
+                        vm.summaryPecentage = Math.round(vm.score * 100);
                         vm.achievementText = '';
                         if (cmpData.hasOwnProperty('achievements')) {
                             for (i = 0; i < cmpData.achievements.length; i++) {
